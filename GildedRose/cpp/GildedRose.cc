440ac4037cbaa66192aec2f8d1835116a17e39ab
Anders Arnholm <Anders@Arnholm.se>
2012-01-25 21:58:31 +0100

    Remodel thet start positions a bit.
    This makes the project a bit more cpp like.
    There is now a .h file for the interface, and all functions inline.
    The .cc file for the implementation.
    Tests are moved to a separate file.

--- GildedRose/cpp/GildedRose.cc
+++ GildedRose/cpp/GildedRose.cc
@@ -1,103 +1,7 @@
-#include <gtest/gtest.h>
-
 #include <string>
+#include <GildedRose.h>
 
 using namespace std;
-
-class Item
-{
-public:
-    string name;
-    int sellIn;
-    int quality;
-    Item(string name, int sellIn, int quality) : name(name), sellIn(sellIn), quality(quality) 
-    {}
-};
-
-class GildedRose
-{
-public:
-    vector<Item> items;
-    GildedRose(vector<Item> items) : items (items) 
-    {}
-    
-    void updateQuality() 
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (items[i].quality > 0)
-                {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].sellIn < 11)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-            {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0)
-            {
-                if (items[i].name != "Aged Brie")
-                {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].quality > 0)
-                        {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                }
-                else
-                {
-                    if (items[i].quality < 50)
-                    {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
-};
-
 
 void example()
 {
@@ -111,13 +15,3 @@ void example()
     GildedRose app(items);
     app.updateQuality();
 }
-
-
-TEST(GildedRoseTest, Foo) {
-    vector<Item> items;
-    items.push_back(Item("Foo", 0, 0));
-    GildedRose app(items);
-    app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
-}
-
---
+++ GildedRose/cpp/GildedRoseTest.cc
@@ -0,0 +1,14 @@
+
+#include <vector>
+#include <gtest/gtest.h>
+#include <GildedRose.h>
+
+
+TEST(GildedRoseTest, Foo) {
+    vector<Item> items;
+    items.push_back(Item("Foo", 0, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("fixme", app.items[0].name);
+}
+
